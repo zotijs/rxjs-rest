573c2d0e84ca980819b0be4b416f1f1b378d4a23
Sotiris Tsiamouris <tsot85@gmail.com>
2019-09-11 00:37:13 +0300

    working with heroku

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ mongoCreate(app);
 
 require("./routes/usersRoutes")(app, upload);
 
+if (process.env.NODE_ENV === "production") {
+  //express will serve production assets like
+  //main.js or/and main.css
+  app.use(express.static("client/build"));
+
+  //express will serve the index.html file
+  //if it doesn't know the route
+  //handle client side (React Router) routes
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
